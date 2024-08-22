a84310f1cb9363d176223f5f48e26b497174bda2
script-js <134544514+script-js@users.noreply.github.com>
2024-08-22 16:02:44 -0700

    Update import.js

--- import.js
+++ import.js
@@ -131,5 +131,19 @@ input[type="color"] {
 	}
       }
     }
+  },
+  editSelection: function(attr) {
+    var sel = window.getSelection(); // Gets selection
+    if (sel.rangeCount) {
+      // Creates a new element, and insert the selected text with the chosen font inside
+      var e = document.createElement('span');
+      e.style = attr
+      e.innerHTML = sel.toString();
+
+      // https://developer.mozilla.org/en-US/docs/Web/API/Selection/getRangeAt
+      var range = sel.getRangeAt(0);
+      range.deleteContents(); // Deletes selected text…
+      range.insertNode(e); // … and inserts the new element at its place
+    }
   }
 }
